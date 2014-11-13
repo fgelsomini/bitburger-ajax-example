53da3b3315f3540f5f1aacfe4df8a7b9d37adb68
Fabio Gelsomini <fgelsomini@gmail.com>
2014-11-13 17:12:53 -0500

    Refactor sync section

--- Gemfile
+++ Gemfile
@@ -29,8 +29,6 @@ gem 'spring',        group: :development
 
 # Use unicorn as the app server
 gem 'unicorn'
-# gem 'thin'
-# gem 'puma'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,25 +1,17 @@
 class OrdersController < ApplicationController
 
-  layout false, except: [:index]
-
   def index
+  end
+
+  def asynchronous
+  end
+
+  def synchronous
   end
 
   def take_order
     sleep params[:items].to_i
     render :nothing => true
-  end
-
-  def cook1
-    sleep 4
-  end
-
-  def cook2
-    sleep 1
-  end
-
-  def cook3
-    sleep 2
   end
 
 end
